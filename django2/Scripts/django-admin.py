5e7c9508307972ee42f3a9401224538d8f56f4d8
Albina Giliazova <giliazova@mail.ru>
2025-04-29 14:58:31 +0300

    final

---
+++ django2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tigrisha\python_tasks\test tasks\cashoff\django2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
